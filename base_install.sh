ba30d3e1b6df87e101d5e277eb82fe79eb0895f9
V0idMatr1x <v0idmatr1x.0@gmail.com>
2024-01-19 12:07:09 -0600

    Update: base_install

--- base_install.sh
+++ base_install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-PKG_LIST="zsh mosh parallel git perl gcc make cmake clang mold lldb llvm strace helix nodejs npm yarnpkg akmod-nvidia nvidia-persistenced xorg-x11-drv-nvidia-cuda akmod-VirtualBox VirtualBox protobuf-compiler podman podman-compose kubernetes jetbrains-mono-fonts alacritty gh btop snapper btrfsmaintenance btrfs-assistant"
+PKG_LIST="zsh mosh parallel git perl opam gcc make cmake clang mold lldb llvm strace helix nodejs npm yarnpkg akmod-nvidia nvidia-persistenced xorg-x11-drv-nvidia-cuda akmod-VirtualBox VirtualBox protobuf-compiler podman podman-compose kubernetes jetbrains-mono-fonts alacritty gh btop snapper btrfsmaintenance btrfs-assistant"
 sudo dnf copr enable varlad/helix
 sudo dnf in $PKG_LIST
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
@@ -8,3 +8,6 @@ rustup default stable
 rustup component add rust-analyzer && rustup target add wasm32-unknown-unknown
 cargo install cargo-edit cargo-audit cargo-geiger cargo-outdated cargo-deny cargo-info cargo-show-asm cargo-expand cargo-license cargo-release cargo-watch cargo-tarpaulin sccache bindgen-cli hyperfine trunk cross ripgrep eza starship slint-lsp  
 cargo install zellij --locked
+cargo install taplo-cli --locked --features lsp
+cargo install --git https://github.com/wgsl-analyzer/wgsl-analyzer wgsl_analyzer
+sudo npm i -g prettier bash-language-server typescript typescript-language-server yaml-language-server@next @ansible/ansible-language-server @tailwindcss/language-server @astrojs/language-server sql-language-server vscode-langservers-extracted dockerfile-language-server-nodejs
