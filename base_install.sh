c638c81cb781b7fcda85d827478069c084be10f5
Chris <88049272+V0idMatr1x@users.noreply.github.com>
2024-01-19 12:38:13 -0600

    Update base_install.sh

--- base_install.sh
+++ base_install.sh
@@ -14,4 +14,5 @@ cargo install zellij --locked
 cargo install taplo-cli --locked --features lsp
 cargo install --git https://github.com/wgsl-analyzer/wgsl-analyzer wgsl_analyzer
 sudo npm i -g prettier bash-language-server typescript typescript-language-server yaml-language-server@next @ansible/ansible-language-server @tailwindcss/language-server @astrojs/language-server sql-language-server vscode-langservers-extracted dockerfile-language-server-nodejs
+chsh
 systemctl reboot -i
