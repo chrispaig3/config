a68c89a07b85017d04bcf0717af4f741617189ad
V0idMatr1x <v0idmatr1x.0@gmail.com>
2024-01-19 11:56:54 -0600

    Update: base_install

--- base_install.sh
+++ base_install.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
-PKG_LIST="zsh parallel git perl gcc make cmake clang lldb llvm strace helix nodejs npm yarnpkg akmod-nvidia nvidia-persistenced xorg-x11-drv-nvidia-cuda akmod-VirtualBox VirtualBox protobuf-compiler podman podman-compose kubernetes jetbrains-mono-fonts alacritty gh btop snapper btrfsmaintenance btrfs-assistant"
+PKG_LIST="zsh mosh parallel git perl gcc make cmake clang mold lldb llvm strace helix nodejs npm yarnpkg akmod-nvidia nvidia-persistenced xorg-x11-drv-nvidia-cuda akmod-VirtualBox VirtualBox protobuf-compiler podman podman-compose kubernetes jetbrains-mono-fonts alacritty gh btop snapper btrfsmaintenance btrfs-assistant"
 sudo dnf copr enable varlad/helix
 sudo dnf in $PKG_LIST
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+source .cargo/env
+rustup default stable
+rustup component add rust-analyzer && rustup target add wasm32-unknown-unknown
+cargo install cargo-edit cargo-audit cargo-geiger cargo-outdated cargo-deny cargo-info cargo-show-asm cargo-expand cargo-license cargo-release cargo-watch cargo-tarpaulin sccache bindgen-cli hyperfine trunk cross ripgrep eza starship slint-lsp  
+cargo install zellij --locked
