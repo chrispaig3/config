533b21693bb8b99b1171ab32672f949c57c403b3
Chris <88049272+V0idMatr1x@users.noreply.github.com>
2024-01-19 13:33:15 -0600

    Update base_install.sh

--- base_install.sh
+++ base_install.sh
@@ -8,7 +8,7 @@
 PKG_LIST="zsh mosh parallel git perl opam gcc make cmake clang mold lldb llvm strace helix nodejs npm yarnpkg akmod-nvidia nvidia-persistenced xorg-x11-drv-nvidia-cuda obs-studio blender gamemode godot lutris winetricks akmod-VirtualBox VirtualBox protobuf-compiler podman podman-compose kubernetes jetbrains-mono-fonts alacritty gh btop snapper btrfsmaintenance btrfs-assistant thunderbird mpv ffmpeg"
 sudo dnf copr enable varlad/helix
 sudo dnf in $PKG_LIST --allowerasing
-git clone https://github.com/ryanoasis/nerd-fonts
+git clone https://github.com/ryanoasis/nerd-fonts.git
 cd nerd-fonts/patched-fonts/NerdFontsSymbolsOnly && sudo mv SymbolsNerdFont* /usr/share/fonts; cd $HOME/config
 mv helix $HOME/.config
 mv alacritty $HOME/.config
