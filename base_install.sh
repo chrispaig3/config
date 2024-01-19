5816cb6e4dd57a96c76c7293e1713d3f12adf833
Chris <88049272+V0idMatr1x@users.noreply.github.com>
2024-01-19 11:57:44 -0600

    Update base_install.sh

--- base_install.sh
+++ base_install.sh
@@ -3,7 +3,7 @@ PKG_LIST="zsh mosh parallel git perl gcc make cmake clang mold lldb llvm strace 
 sudo dnf copr enable varlad/helix
 sudo dnf in $PKG_LIST
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-source .cargo/env
+source $HOME/.cargo/env
 rustup default stable
 rustup component add rust-analyzer && rustup target add wasm32-unknown-unknown
 cargo install cargo-edit cargo-audit cargo-geiger cargo-outdated cargo-deny cargo-info cargo-show-asm cargo-expand cargo-license cargo-release cargo-watch cargo-tarpaulin sccache bindgen-cli hyperfine trunk cross ripgrep eza starship slint-lsp  
