2c0ca2b819d2160f993e83439b3d8e5b886f4ccc
Chris <88049272+V0idMatr1x@users.noreply.github.com>
2024-01-19 12:35:58 -0600

    Update base_install.sh

--- base_install.sh
+++ base_install.sh
@@ -2,6 +2,9 @@
 PKG_LIST="zsh mosh parallel git perl opam gcc make cmake clang mold lldb llvm strace helix nodejs npm yarnpkg akmod-nvidia nvidia-persistenced xorg-x11-drv-nvidia-cuda akmod-VirtualBox VirtualBox protobuf-compiler podman podman-compose kubernetes jetbrains-mono-fonts alacritty gh btop snapper btrfsmaintenance btrfs-assistant thunderbird mpv ffmpeg"
 sudo dnf copr enable varlad/helix
 sudo dnf in $PKG_LIST --allowerasing
+git clone https://github.com/v0idmatr1x/config.git
+mv config/helix .config
+mv config/alacritty .config
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 source $HOME/.cargo/env
 rustup default stable
