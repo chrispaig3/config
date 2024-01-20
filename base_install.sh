43c91ce5df80f400660338cb16b627f8512e5009
Chris <88049272+V0idMatr1x@users.noreply.github.com>
2024-01-20 06:25:43 -0600

    Update base_install.sh

--- base_install.sh
+++ base_install.sh
@@ -5,7 +5,7 @@
 # Author: V0idMatr1x - Chris 
 # Date: 1/19/2024
 
-PKG_LIST="zsh mosh parallel perl python-devel fluidsynth-devel opam gcc make cmake clang mold lldb llvm strace helix nodejs npm yarnpkg akmod-nvidia nvidia-persistenced xorg-x11-drv-nvidia-cuda obs-studio blender gamemode lutris winetricks godot akmod-VirtualBox VirtualBox protobuf-compiler podman podman-compose kubernetes jetbrains-mono-fonts alacritty gh btop snapper btrfsmaintenance btrfs-assistant thunderbird mpv ffmpeg"
+PKG_LIST="zsh mosh parallel perl python-devel fluidsynth-devel qbittorrent opam gcc make cmake clang mold lldb llvm strace helix nodejs npm yarnpkg akmod-nvidia nvidia-persistenced xorg-x11-drv-nvidia-cuda obs-studio blender gamemode lutris winetricks godot akmod-VirtualBox VirtualBox protobuf-compiler podman podman-compose kubernetes jetbrains-mono-fonts alacritty gh btop snapper btrfsmaintenance btrfs-assistant thunderbird mpv ffmpeg"
 sudo dnf copr enable varlad/helix
 sudo dnf in $PKG_LIST --allowerasing
 git clone https://github.com/ryanoasis/nerd-fonts.git
