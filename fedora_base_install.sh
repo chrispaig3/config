416c106976fbf016134b5e4cf1025e58c18b53d3
Chris Paige <88049272+V0idMatr1x@users.noreply.github.com>
2024-03-19 15:18:13 -0500

    Update fedora_base_install.sh

--- fedora_base_install.sh
+++ fedora_base_install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 sudo dnf rm vi kmail konsole
-PKG_LIST="zsh gh mullvad-vpn mosh parallel perl sqlite-devel python-devel python-lsp-server qbittorrent opam gcc valgrind make cmake clang doxygen mold lldb llvm strace binwalk helix nodejs npm yarnpkg akmod-nvidia nvidia-persistenced xorg-x11-drv-nvidia-cuda obs-studio blender akmod-VirtualBox VirtualBox protobuf-compiler podman podman-compose kubernetes jetbrains-mono-fonts alacritty btop snapper btrfsmaintenance btrfs-assistant thunderbird mpv ffmpeg"
+PKG_LIST="zsh gh mullvad-vpn mosh parallel perl sqlite-devel python-devel python-lsp-server apr apr-util libxcrypt-compat gamemode wireshark qbittorrent opam gcc valgrind make cmake clang doxygen mold lldb llvm strace binwalk helix nodejs npm yarnpkg akmod-nvidia nvidia-persistenced xorg-x11-drv-nvidia-cuda obs-studio blender akmod-VirtualBox VirtualBox protobuf-compiler podman podman-compose kubernetes jetbrains-mono-fonts alacritty btop snapper btrfsmaintenance btrfs-assistant thunderbird mpv ffmpeg"
 sudo dnf copr enable varlad/helix
 sudo dnf config-manager --add-repo https://repository.mullvad.net/rpm/stable/mullvad.repo
 sudo dnf in $PKG_LIST --allowerasing
