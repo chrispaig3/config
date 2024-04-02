8d0dbb63fca01f85a9d50d559bb17034513cae58
V0idMatr1x <v0idmatr1x.0@gmail.com>
2024-04-02 15:06:48 -0500

    Update

--- fedora_base_install.sh
+++ fedora_base_install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 sudo dnf rm vi kmail konsole
-PKG_LIST="zsh gh mullvad-vpn mosh parallel perl sqlite-devel python-devel python-lsp-server apr apr-util libxcrypt-compat gamemode wireshark qbittorrent opam gcc valgrind make cmake clang doxygen mold lldb llvm strace binwalk helix nodejs npm yarnpkg akmod-nvidia nvidia-persistenced xorg-x11-drv-nvidia-cuda obs-studio blender akmod-VirtualBox VirtualBox protobuf-compiler podman podman-compose kubernetes jetbrains-mono-fonts alacritty btop snapper btrfsmaintenance btrfs-assistant thunderbird mpv ffmpeg"
+PKG_LIST="zsh gh mullvad-vpn mosh parallel perl sqlite-devel python-devel python-lsp-server apr apr-util libxcrypt-compat gamemode wireshark qbittorrent opam gcc valgrind make cmake clang doxygen mold lldb llvm strace binwalk helix nodejs npm yarnpkg akmod-nvidia nvidia-persistenced xorg-x11-drv-nvidia-cuda obs-studio akmod-VirtualBox VirtualBox protobuf-compiler podman podman-compose kubernetes jetbrains-mono-fonts alacritty btop snapper btrfsmaintenance btrfs-assistant thunderbird mpv ffmpeg"
 sudo dnf copr enable varlad/helix
 sudo dnf config-manager --add-repo https://repository.mullvad.net/rpm/stable/mullvad.repo
 sudo dnf in $PKG_LIST --allowerasing
@@ -12,11 +12,10 @@ source $HOME/.cargo/env
 rustup default stable
 mv cargo/config.toml $HOME/.cargo
 rustup component add rust-analyzer && rustup target add wasm32-unknown-unknown
-cargo install cargo-edit cargo-audit cargo-geiger cargo-outdated cargo-deny cargo-generate cargo-info cargo-show-asm cargo-expand cargo-about cargo-license cargo-release cargo-watch cargo-tarpaulin cargo-shuttle sccache bpf-linker bindgen-cli hyperfine trunk ripgrep eza starship slint-lsp  
+cargo install cargo-edit cargo-audit cargo-geiger cargo-outdated cargo-deny cargo-generate cargo-info cargo-show-asm cargo-expand cargo-about cargo-license cargo-release cargo-watch cargo-tarpaulin cargo-shuttle sccache bpf-linker bindgen-cli hyperfine trunk ripgrep nushell slint-lsp  
 cargo install cargo-feature --locked
 cargo install zellij --locked
 cargo install taplo-cli --locked --features lsp
 cargo install --git https://github.com/wgsl-analyzer/wgsl-analyzer wgsl_analyzer
 sudo npm i -g prettier jsdoc bash-language-server pyright typescript typescript-language-server yaml-language-server@next @ansible/ansible-language-server @tailwindcss/language-server @astrojs/language-server sql-language-server vscode-langservers-extracted dockerfile-language-server-nodejs
-chsh
 systemctl reboot -i
